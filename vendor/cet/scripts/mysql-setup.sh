ba4fe45d5daafaebe09470bc9e42bbe383b14e2f
sulph8 <sulph8@gmail.com>
2016-11-29 11:19:16 +0100

    Added some logging for containers

--- vendor/cet/scripts/mysql-setup.sh
+++ vendor/cet/scripts/mysql-setup.sh
@@ -4,7 +4,7 @@ echo "=> Importing dashboard database"
 mysql -uroot -e "create database dashboard;"
 mysql -uroot -Ddashboard < /var/www/html/vendor/cet/scripts/dashboard.sql
 
-echo "=> Importing kayako database"
+echo "=> Importing kayako database (this could take some time, please be patient"
 mysql -uroot -e "create database kayako;"
 mysql -uroot -Dkayako < /var/www/html/vendor/cet/scripts/kayako.sql
 
@@ -15,4 +15,6 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION"
 if [ ! -f /var/www/html/vendor/cet/config.inc.php ] ; then
 	echo "=> Intializing sample config"
 	cp -r /var/www/html/vendor/cet/config.sample.inc.php /var/www/html/vendor/cet/config.inc.php
+	echo "=> Disableing authentication"
+	sed -i '4s/1/0/' /var/www/html/vendor/cet/config.inc.php
 fi
