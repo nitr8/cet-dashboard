b25d4e04945240df8814e42a3bab133ad4311db0
lukaszdrahal <lzdrahal@outlook.com>
2016-12-20 12:45:33 +0100

    added tickets per year for departments

--- content/managment/ticketsbyyear.php
+++ content/managment/ticketsbyyear.php
@@ -1,12 +1,78 @@
-<div class="wrapper wrapper-content animated fadeInRight">
-    <div class="row">
-          <div class="col-lg-12 ibox-title">
-              <h2> Tickets logged in last years (department - cet) </h2>
-			  <div class="col-lg-2">
-                    <div class="ibox float-e-margins">
+<?php 
+function generateDataString($sql,$db)
+{
+	$datastring = "";
 
+	$rows = $db->query($sql);
+	$index = 0;
+	while ($record = $db->fetch($rows))
+	{
+	if ($index >0)$datastring.= " ,\n";
+		$datastring.= "\"";
+		$datastring.= $record["date"];
+		$datastring.= "\":\"";
+		$datastring.= $record["cnt"];
+		$datastring.= "\"";
+		$index++;
+	}
+	return $datastring;	
+}
+
+$department = "CET";
+if(isset($_GET['department']))
+	$department = $_GET['department'];
+
+$sqlQueryCET = "SELECT DATE_FORMAT(from_unixtime(t.dateline),'%Y-%m-%d') as date, count(DATE_FORMAT(from_unixtime(t.dateline),'%Y-%m-%d')) as cnt FROM ".KSQL_TPRFX."tickets t where t.departmenttitle='CET' group by date";
+$sqlQueryQFE = "SELECT DATE_FORMAT(from_unixtime(t.dateline),'%Y-%m-%d') as date, count(DATE_FORMAT(from_unixtime(t.dateline),'%Y-%m-%d')) as cnt FROM ".KSQL_TPRFX."tickets t where t.departmenttitle='QFE' group by date";
+$sqlQueryIT = "SELECT DATE_FORMAT(from_unixtime(t.dateline),'%Y-%m-%d') as date, count(DATE_FORMAT(from_unixtime(t.dateline),'%Y-%m-%d')) as cnt FROM ".KSQL_TPRFX."tickets t where t.departmenttitle='IT' group by date";
+	$sqlQuery = $sqlQueryCET;
+switch ($department)	
+{
+	case "CET" : 
+		$sqlQuery = $sqlQueryCET;
+		break;
+	case "IT" : 
+		$sqlQuery = $sqlQueryIT;
+		break;
+	case "QFE" : 
+		$sqlQuery = $sqlQueryQFE;
+		break;
+}
+?>
+
+<div class="wrapper wrapper-content">
+    <div class="row">
+        <div class="col-lg-12 ibox-title">
+            <h2>  Tickets logged in last years (department - <?php echo $department;?>)  </h2>
+			
+			<div class="row">
+				<div class="col-sm-4">
+					<select name="week" class="form-control m-b" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+					<option value ="?page=managment_ticketsbyyear&department=CET" <?php echo $department=="CET"?"SELECTED":"";?>>CET</option>
+					<option value ="?page=managment_ticketsbyyear&department=QFE" <?php echo $department=="QFE"?"SELECTED":"";?>>QFE</option>
+					<option value ="?page=managment_ticketsbyyear&department=IT" <?php echo $department=="IT"? "SELECTED":"";?>>IT</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-lg-12">
+                <div id="tpy" class="ibox float-e-margins"></div>
+            </div>
+		</div>
+	</div>
+</div>
                   
 <script>
+
+function monthPath(t0) {
+  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+      d0 = t0.getDay(), w0 = d3.time.weekOfYear(t0),
+      d1 = t1.getDay(), w1 = d3.time.weekOfYear(t1);
+  return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
+      + "H" + w0 * cellSize + "V" + 7 * cellSize
+      + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
+      + "H" + (w1 + 1) * cellSize + "V" + 0
+      + "H" + (w0 + 1) * cellSize + "Z";
+}
 
 var width = 960,
     height = 136,
@@ -19,9 +85,9 @@ var color = d3.scale.quantize()
     .domain([-10, 10])
     .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
 
-	var divi = d3.select("body").select("div").select("div");
-var svg = divi.selectAll("svg")
-    .data(d3.range(2013, 2017)) 
+//CET////////////////////////////////////////////////////////////////////////
+var svg = d3.select("body").select("#tpy").selectAll("svg")
+    .data(d3.range( <?php echo $department=="QFE"?"2015":"2013";?>, 2017)) 
   .enter().append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -53,38 +119,16 @@ svg.selectAll(".month")
     .attr("class", "month")
     .attr("d", monthPath);
 
-d3.csv("dji.csv", function(error, csv) {
-  if (error) throw error;
-
-  var data = d3.nest()
-    .key(function(d) { return d.Date; })
-    .rollup(function(d) { return d[0].Count; })
-    .map(csv);
-
+var data =  {<?php echo generateDataString($sqlQuery,$db);?>};
   rect.filter(function(d) { return d in data; })
       .attr("class", function(d) { return "day " + color(data[d]); })
     .select("title")
       .text(function(d) { return d + ": " + percent(data[d]); });
-});
 
-function monthPath(t0) {
-  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-      d0 = t0.getDay(), w0 = d3.time.weekOfYear(t0),
-      d1 = t1.getDay(), w1 = d3.time.weekOfYear(t1);
-  return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
-      + "H" + w0 * cellSize + "V" + 7 * cellSize
-      + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
-      + "H" + (w1 + 1) * cellSize + "V" + 0
-      + "H" + (w0 + 1) * cellSize + "Z";
-}
+	  
+	  
 
 </script>
 
-                        </div>
-                    </div>
-					</div>
-               </div>
-		</div>
-	</div>
-</div>		
+
 
