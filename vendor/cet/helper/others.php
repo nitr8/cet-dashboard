bcf7ff89e88bd38c7cc1c779b714ccf830f0c53e
lukaszdrahal <lzdrahal@outlook.com>
2017-01-16 18:44:04 +0100

    added petabyte suffix

--- vendor/cet/helper/others.php
+++ vendor/cet/helper/others.php
@@ -256,7 +256,7 @@ function formatBytes($size, $precision = 2)
 	if($size == 0)
 	return "0 B";
     $base = log($size, 1024);
-    $suffixes = array('', 'KiB', 'MiB', 'GiB', 'TiB');   
+    $suffixes = array('', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB');   
  
     return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
 }
